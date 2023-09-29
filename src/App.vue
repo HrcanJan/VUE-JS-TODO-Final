e4d1888f7ee74ceded133011b4bf03a6d3a2206e
Hrćan Jan <xhrcan@stuba.sk>
2023-09-29 16:58:14 +0200

    feat: transfered app.vue to vuex

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,31 @@
 <template>
-    <div class="bg"></div>
     <ul>
-        <li class="active" id="list" @click="activeTab('list')">
-            <router-link to="/">List({{ this.itemAmount }})</router-link>
+        <li class="active" id="list" @click="$store.commit('activeTab', 'list')">
+            <router-link to="/">List({{ $store.state.activeItemsCount }})</router-link>
         </li>
-        <li id="deleted" @click="activeTab('deleted')">
-            <router-link to="/deleted">Deleted({{ this.deletedAmount }})</router-link>
+        <li id="deleted" @click="$store.commit('activeTab', 'deleted')">
+            <router-link to="/deleted">Deleted({{ $store.state.deletedItemsCount }})</router-link>
         </li>
-        <li id="deleted" @click="getData()">
+        <li id="deleted" @click="$store.dispatch('getData')">
             <a>GET</a>
         </li>
-        <li id="deleted" @click="postData()">
+        <li id="deleted" @click="$store.commit('postData')">
             <a>POST</a>
         </li>
     </ul>
     <div class="wrapper">
-        <router-view 
-            :item_list="this.items"
-            :postData="postData"
-            :putData="putData"
-            :itemAmount="updateItemAmount"
-            :deletedAmount="updateDeletedAmount"
-        />
+        <router-view />
     </div>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
-	data() {
-		return {
-			items: [],
-            deletedAmount: 0,
-            itemAmount: 0
-		}
-	},
-
     async mounted() {
         try{
-            this.getData()
+            this.$store.commit('getData')
         } catch (error) {
             console.error("Error fetching data:", error);
             }
-    },
-
-    methods: {
-        activeTab(id) {
-		    const tabs = document.getElementsByTagName('li')
-		    const activeTab = document.getElementById(id)
-
-	    for (let tab of tabs)
-		    tab.classList.remove('active')
-
-	    activeTab.classList.add('active')
-	    },
-
-        getData() {
-            axios.get('https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/items')
-            .then(response => {
-                this.items = JSON.parse(JSON.stringify(response.data))
-                console.log(response.data)
-            })
-        },
-
-        postData(itemId = 4, itemName = "Date", itemIsDeleted = false) {
-            axios({
-                method: 'post',
-                url: 'https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/additems',
-                data: {"id": itemId, "name": itemName, "isDeleted": itemIsDeleted}
-                })
-            .then(response => console.log(response.data))
-        },
-
-        putData(itemId, itemIsDeleted) {
-            axios({
-                method: 'put',
-                url: 'https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/deleteitems',
-                data: {"id": itemId, "isDeleted": itemIsDeleted}
-                })
-            .then((response) => console.log(response.data))
-        },
-
-        updateItemAmount(amount = 0){
-            this.itemAmount = amount
-        },
-
-        updateDeletedAmount(amount = 0){
-            this.deletedAmount = amount
-        }
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
     state: {
@@ -11,8 +12,55 @@ export default createStore({
     getters: {
     },
     mutations: {
+        activeTab(id) {
+		    const tabs = document.getElementsByTagName('li')
+		    const activeTab = document.getElementById(id)
+
+            for (let tab of tabs)
+                tab.classList.remove('active')
+
+            activeTab.classList.add('active')
+	    },
+
+        getData(state, data) {
+            state.items = data
+        },
+
+        postData(itemId = 4, itemName = "Date", itemIsDeleted = false) {
+            axios({
+                method: 'post',
+                url: 'https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/additems',
+                data: {"id": itemId, "name": itemName, "isDeleted": itemIsDeleted}
+                })
+            .then(response => console.log(response.data))
+        },
+
+        putData(itemId, itemIsDeleted) {
+            axios({
+                method: 'put',
+                url: 'https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/deleteitems',
+                data: {"id": itemId, "isDeleted": itemIsDeleted}
+                })
+            .then((response) => console.log(response.data))
+        },
+
+        updateItemAmount(state, amount = 0){
+            state.activeItemsCount = amount
+        },
+
+        updateDeletedAmount(amount = 0){
+            state.deletedItemsCount = amount
+        }
     },
     actions: {
+        getData() {
+            axios.get('https://28ad3fcf-e1e0-48be-b014-13f6120e1bc0.mock.pstmn.io/items')
+            .then(response => {
+                const data = JSON.parse(JSON.stringify(response.data))
+                console.log(data)
+                commit('getData', data)
+            })
+        }
     },
     modules: {
     }
