42b93b6b8437469df3690037b62108b19142998d
Hrćan Jan <xhrcan@stuba.sk>
2023-09-29 16:02:16 +0200

    feat: fill state in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        deletedItemsCount : 0,
+        activeItemsCount : 0,
+        items : [],
+        input : '',
+        isDeleted : false
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
 })
