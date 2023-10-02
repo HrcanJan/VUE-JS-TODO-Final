b2ec17c3a4ca97ff44d9ef1c6beba553a3095f81
Hrćan Jan <xhrcan@stuba.sk>
2023-10-02 12:43:18 +0200

    feat: edit button togge input

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -6,13 +6,18 @@
                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
             </svg>
         </span>
-        <span style="margin-right: 15px" class="itemButton">
+        <span @click="editItem" style="margin-right: 15px" class="itemButton">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
         </span>
-        <span>{{  item.name }}</span>
+        <div :class="this.inputDiv">
+            <input name="input" :value="this.item.name" />
+            <button>Save</button>
+            <button>Cancel</button>
+        </div>
+        <span :class="this.itemNameDiv">{{  item.name }}</span>
     </div>
     <div class="item" v-if="item.isDeleted && $store.state.isDeleted">
         <span @click="$store.commit('undeleteItem', item)" style="margin-right: 15px" class="itemButton">
@@ -27,8 +32,23 @@
 <script>
 export default {
     name: 'Item',
+
+    data() {
+		return {
+			inputDiv: "inactiveDiv",
+            itemNameDiv: "",
+		}
+	},
+
     props: {
         item: Object,
     },
+
+    methods: {
+        editItem(){
+            this.inputDiv = ""
+            this.itemNameDiv = "inactiveDiv"
+        }
+    }
 }
 </script>
