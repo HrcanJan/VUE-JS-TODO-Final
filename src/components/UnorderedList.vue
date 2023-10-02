45dd941621a6cb04f4e86fea33fd0fe14c7cf4d8
Hrćan Jan <xhrcan@stuba.sk>
2023-10-02 12:04:25 +0200

    commit: add Edit, Delete and Undelete Buttons

--- src/components/UnorderedList.vue
+++ src/components/UnorderedList.vue
@@ -1,10 +1,10 @@
 <template>
     <ul>
         <li class="active" id="list" @click="$store.commit('activeTab', 'list')">
-            <router-link to="/">List({{ $store.state.activeItemsCount }})</router-link>
+            <router-link to="/">List ({{ $store.state.activeItemsCount }})</router-link>
         </li>
         <li id="deleted" @click="$store.commit('activeTab', 'deleted')">
-            <router-link to="/deleted">Deleted({{ $store.state.deletedItemsCount }})</router-link>
+            <router-link to="/deleted">Deleted ({{ $store.state.deletedItemsCount }})</router-link>
         </li>
         <li id="deleted" @click="$store.dispatch('getData')">
             <a>GET</a>
