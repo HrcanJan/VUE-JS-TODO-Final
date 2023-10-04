28eefbe6ac554758edf782071144edf06c6c5af0
Hrćan Jan <xhrcan@stuba.sk>
2023-10-04 15:14:19 +0200

    fix: list of active/deleted items via computed

--- src/components/UnorderedList.vue
+++ src/components/UnorderedList.vue
@@ -1,10 +1,10 @@
 <template>
     <ul>
         <li @click="$store.commit('activeTab', 'list')">
-            <router-link to="/">List ({{ $store.state.activeItemsCount }})</router-link>
+            <router-link to="/">List ({{ activeItemsCount }})</router-link>
         </li>
         <li @click="$store.commit('activeTab', 'deleted')">
-            <router-link to="/deleted">Deleted ({{ $store.state.deletedItemsCount }})</router-link>
+            <router-link to="/deleted">Deleted ({{ deletedItemsCount }})</router-link>
         </li>
         <li @click="$store.dispatch('getData')">
             <a>GET</a>
@@ -17,6 +17,27 @@
 
 <script>
 export default{
-    
+    computed: {
+        activeItemsCount(){
+            const data = this.$store.state.items
+            let activeItems = 0
+
+            for(let i = 0; i < data.length; i++)
+				if(!data[i].isDeleted)
+					activeItems++
+			return activeItems
+        },
+
+        deletedItemsCount(){
+            const data = this.$store.state.items
+            let deletedItems = 0
+
+            for(let i = 0; i < data.length; i++)
+				if(data[i].isDeleted)
+					deletedItems++
+			return deletedItems
+
+        }
+    }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 
 export default createStore({
     state: {
-        deletedItemsCount : 0,
-        activeItemsCount : 0,
         items : [],
         input : '',
         isDeleted : false
@@ -33,7 +31,6 @@ export default createStore({
             for(let i = 0; i < data.length; i++)
                 if(itemNames.indexOf(data[i].name) === -1)
                     state.items.push(data[i])
-            this.commit('calculateAmount')
         },
 
         postData(state, itemId = 4, itemName = "Date", itemIsDeleted = false) {
@@ -54,14 +51,6 @@ export default createStore({
             .then((response) => console.log(response.data))
         },
 
-        updateItemAmount(state, amount = 0){
-            state.activeItemsCount = amount
-        },
-
-        updateDeletedAmount(state, amount = 0){
-            state.deletedItemsCount = amount
-        },
-
         addItem(state) {
 			if(state.input) {
 				state.items.push({
@@ -72,20 +61,17 @@ export default createStore({
 				this.commit('postData', state.items.length, state.input, false)
 				state.input = ''
 				this.commit('isInput')
-				this.commit('calculateAmount')
 			}
 		},
 
 		deleteItem(state, item) {
 			item.isDeleted = true
 			this.commit('putData', item)
-			this.commit('calculateAmount')
 		},
 
         undeleteItem(state, item) {
 			item.isDeleted = false
 			this.commit('putData', item)
-			this.commit('calculateAmount')
 		},
 
 		isInput(state) {
@@ -94,19 +80,6 @@ export default createStore({
 				button.disabled = false
 			else
 				button.disabled = true
-		},
-
-		calculateAmount(state) {
-			let activeItems = 0
-			let inactiveItems = 0
-			for(let i = 0; i < state.items.length; i++){
-				if(!state.items[i].isDeleted)
-					activeItems++
-				else
-					inactiveItems++
-			}
-			this.commit('updateItemAmount', activeItems)
-			this.commit('updateDeletedAmount', inactiveItems)
 		}
     },
     actions: {
